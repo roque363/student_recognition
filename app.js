938ec8953c3c3e08648e81da7e341f1851cee8bb
Roque Alarcon <roquealarcon363@gmail.com>
2018-06-20 15:47:25 +0000

    Se agrego diseño

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var firebase = require("firebase");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCHfnjNm-3t43KqZukO4UvgnW4Puivi7xo",
+  authDomain: "recognizedios.firebaseapp.com",
+  databaseURL: "https://recognizedios.firebaseio.com",
+  projectId: "recognizedios",
+  storageBucket: "recognizedios.appspot.com",
+  messagingSenderId: "251451876031"
+};
+firebase.initializeApp(config);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,9 +34,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/prueba',function(req,res) {
+  res.render('prueba');
+});
+
+app.get('/login',function(req,res) {
+  res.render('login');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.render('page-error-404');
 });
 
 // error handler
